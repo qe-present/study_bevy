934d63bb999f6e2b3e28209ae3eaf95257bd0be9
qe-present <2664481691@qq.com>
2025-05-13 21:53:51 +0800

    bevy和tauri

---
+++ 03-bevy-egui/src/main.rs
@@ -0,0 +1,18 @@
+use bevy::prelude::*;
+use bevy_egui::{egui, EguiContextPass, EguiContexts, EguiPlugin};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(EguiPlugin {
+            enable_multipass_for_primary_context: true,
+        })
+        .add_systems(EguiContextPass, ui_example_system)
+        .run();
+}
+
+fn ui_example_system(mut contexts: EguiContexts) {
+    egui::Window::new("Hello").show(contexts.ctx_mut(), |ui| {
+        ui.label("world");
+    });
+}
